6b3c0a2b8fb7be4944215bf40771f91dec1cd393
Jevgenij <sssgustavsss@gmail.com>
2022-03-15 14:09:46 +0200

    2.1

--- part1/course_information/src/Content.js
+++ part1/course_information/src/Content.js
@@ -5,7 +5,7 @@ const Content = (props) =>
     const arr = [];
     for (let i = 0; i < props.parts.length; i++)
     {
-        const temp = <Part name={props.parts[i].name} number_of_exercises={props.parts[i].exercises} />
+        const temp = <Part key={i} name={props.parts[i].name} number_of_exercises={props.parts[i].exercises} />
         arr.push(temp)
     }
     return (
