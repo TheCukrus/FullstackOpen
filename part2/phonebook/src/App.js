8920c45d2a17a96c9a7d2e51cd1938a2cd327ac1
Jevgenij <sssgustavsss@gmail.com>
2022-03-18 11:12:06 +0200

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,11 +4,12 @@ const App = () =>
 {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState("")
 
   const handle_click = (event) =>
   {
     event.preventDefault()
-  
+
     for (let i = 0; i < persons.length; i++)
     {
       if (newName === persons[i].name)
@@ -18,32 +19,33 @@ const App = () =>
 
     }
     const temp_arr = [...persons]
-    temp_arr.push({ name: newName });
+    temp_arr.push({ name: newName, number: newNumber });
     setPersons(temp_arr);
   }
 
-  const handle_change = (event) =>
+  const handle_name_change = (event) =>
   {
 
     setNewName(event.target.value);
   }
 
+  const handle_number_change = (event) =>
+  {
+    setNewNumber(event.target.value)
+  }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onChange={handle_change} onSubmit={handle_click}>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit" >add</button>
-        </div>
+      <form onSubmit={handle_click} >
+        <div>name: <input onChange={handle_name_change} /></div>
+        <div>number: <input onChange={handle_number_change} /></div>
+        <div><button type="submit" >add</button></div>
       </form>
       <h2>Numbers</h2>
 
-      {persons.map((ele, i) => <p key={i}>{ele.name}</p>)}
+      {persons.map((ele, i) => <p key={i}>{ele.name} {ele.number}</p>)}
     </div>
   )
 }
