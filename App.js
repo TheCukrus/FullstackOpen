98d95dffe53bc7522e344dc50511296eafea6d35
Jevgenij <sssgustavsss@gmail.com>
2022-03-25 10:42:55 +0200

    testas9

--- App.js
+++ App.js
@@ -4,5 +4,5 @@ const app = new express();
 
 app.use(express.static("./part2/phonebook/build"));
 
-console.log("prievadas", process.env.PORT)
-app.listen(process.env.PORT)
+console.log("prievadas", process.env.PORT||80)
+app.listen(process.env.PORT||80)
