7c378b15cfe0682619f7c231c1336f641578994b
Jevgenij <sssgustavsss@gmail.com>
2022-04-01 15:39:11 +0300

    temp4

--- part2/phonebook/frontend/src/Add_to_phonebook.js
+++ part2/phonebook/frontend/src/Add_to_phonebook.js
@@ -55,7 +55,7 @@ const Add_to_phonebook = (props) =>
 
     return (
         <div>
-            <form onClick={handle_click} >
+            <form onSubmit={handle_click} >
                 <div>name: <input onChange={props.handle_name_change} /></div>
                 <div>number: <input onChange={props.handle_number_change} /></div>
                 <div><button type="submit" >add</button></div>
